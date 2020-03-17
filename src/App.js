6138c7d84c19d7926a0130379ef0deebb55b5edb
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-17 12:35:44 -0700

    Add hero markup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import Nav from './components/Nav';
+import Hero from './components/Hero';
 
 import './layout/config/_base.sass';
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="app">
         <Nav />
+        <Hero />
       </div>
     );
   }
---
+++ src/components/Hero.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+import heroImage from '../assets/hero-image.svg';
+
+// import '../layout/components/hero.sass';
+
+class Hero extends Component {
+  render() {
+    return(
+      <section className="hero">
+        <h1 className="hero__header">
+          Lock in the Time-Value of Your Money
+        </h1>
+        <h4 className="hero__subheader">
+          Trustless Fund is an advanced timelock for ETH and ERC-20 assets. 
+        </h4>
+        <img src={heroImage} alt="Trustless Fund Interface" />
+      </section>
+    );
+  }
+}
+
+export default Hero;
