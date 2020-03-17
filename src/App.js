00b0af96dd65a4a47f041f5621ea567e84583cfd
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-17 12:06:42 -0700

    Clone react boilerplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './layout/config/_base.sass';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Boilerplate</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
