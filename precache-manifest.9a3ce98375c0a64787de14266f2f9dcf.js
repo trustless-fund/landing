8e556dbdfdcea889e2e8b543b5df8adbff09051c
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-20 10:14:47 -0700

    Updates

---
+++ precache-manifest.9a3ce98375c0a64787de14266f2f9dcf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8583d35a95a17a4ddb13e66cc85327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ec1be5f0ae49e1717a0",
+    "url": "/static/css/main.f4935744.chunk.css"
+  },
+  {
+    "revision": "de3e236785fded5a9daf",
+    "url": "/static/js/2.ef5a94fa.chunk.js"
+  },
+  {
+    "revision": "2ec1be5f0ae49e1717a0",
+    "url": "/static/js/main.e3053961.chunk.js"
+  },
+  {
+    "revision": "1732ba32d2accfd5f016",
+    "url": "/static/js/runtime-main.2312a126.js"
+  },
+  {
+    "revision": "d0fa6e8a17bd8f31032321c41c7065b6",
+    "url": "/static/media/github.d0fa6e8a.svg"
+  },
+  {
+    "revision": "b2851be812c0f25d72ea306efff6f468",
+    "url": "/static/media/hero-image.b2851be8.svg"
+  },
+  {
+    "revision": "c0b18cd8e2e98e009ca919ea68d8ff11",
+    "url": "/static/media/lock.c0b18cd8.png"
+  },
+  {
+    "revision": "72fadc36433844523c2837249f726e02",
+    "url": "/static/media/twitter.72fadc36.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.214191c5b526bc9c1851b18334a5bc96.js"
+  "/precache-manifest.9a3ce98375c0a64787de14266f2f9dcf.js"
 );
 
 self.addEventListener('message', (event) => {
