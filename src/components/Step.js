8873f78faa8983f22c6ed272d986faab2a84e3e3
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-17 16:13:57 -0700

    Improve styling of steps

--- src/components/Step.js
+++ src/components/Step.js
@@ -8,18 +8,20 @@ class Step extends Component {
       <section 
         className="step"
         id={this.props.id ? 'learn-more' : null}>
-        <div className="step__side">
-          <h2 className="step__header">
-            {this.props.header}
-          </h2>
-          <p className="step__info">
-            {this.props.info}
-          </p>
+        <div className="step__content">
+          <div className="step__side">
+            <h2 className="step__header">
+              {this.props.header}
+            </h2>
+            <p className="step__info">
+              {this.props.info}
+            </p>
+          </div>
+          <img 
+            src={this.props.image}
+            alt={this.props.alt}
+            className={`step__image${this.props.imageClass ? this.props.imageClass : ''}`} />
         </div>
-        <img 
-          src={this.props.image}
-          alt={this.props.alt}
-          className={`step__image${this.props.imageClass ? this.props.imageClass : ''}`} />
       </section>
     );
   }
