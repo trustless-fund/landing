b0be0684c8509abbb2e20fc671abed89c2f52572
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-17 14:13:35 -0700

    Update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import heroImage from '../assets/hero-image.svg';
+import heroImage from '../assets/hero-image.png';
 
 import '../layout/components/hero.sass';
 
@@ -8,15 +8,24 @@ class Hero extends Component {
   render() {
     return(
       <section className="hero">
-        <h1 className="hero__header">
-          Lock in the Time-Value of Your Money
-        </h1>
-        <h4 className="hero__subheader">
-          Trustless Fund is an advanced timelock for ETH and ERC-20 assets. 
-        </h4>
+        <div className="hero__left">
+          <h1 className="hero__header">
+            Lock in the Time-Value of Your Money
+          </h1>
+          <h4 className="hero__subheader">
+            Trustless Fund is an advanced timelock for ETH and ERC-20 assets. 
+          </h4>
+          <a 
+            href="https://app.trustless.fund/"
+            className="hero__button"
+            target="_blank"
+            rel="noopener noreferrer">
+            Create Fund
+          </a>
+        </div>
         <img 
           src={heroImage} 
-          alt="Trustless Fund Interface" 
+          alt="Trustless Fund" 
           className="hero__image" />
       </section>
     );
