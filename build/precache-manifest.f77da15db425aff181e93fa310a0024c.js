c47da24d3273d81b649448d858211de26b320640
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-20 14:13:11 -0700

    Include build folder in git

---
+++ build/precache-manifest.f77da15db425aff181e93fa310a0024c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f1887f5e31221c8718ec1d029f3875",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9741f613b66526c6e748",
+    "url": "/static/css/main.82cf3ce9.chunk.css"
+  },
+  {
+    "revision": "93f186dd10e2540ef761",
+    "url": "/static/js/2.d4360338.chunk.js"
+  },
+  {
+    "revision": "9741f613b66526c6e748",
+    "url": "/static/js/main.1bb703f3.chunk.js"
+  },
+  {
+    "revision": "1732ba32d2accfd5f016",
+    "url": "/static/js/runtime-main.2312a126.js"
+  },
+  {
+    "revision": "cfa5089904a3a9ed713ae1e9c69a03b6",
+    "url": "/static/media/calendar.cfa50899.png"
+  },
+  {
+    "revision": "087f9d2fe056c53cb82aadb0bbd8107e",
+    "url": "/static/media/discord.087f9d2f.png"
+  },
+  {
+    "revision": "d0fa6e8a17bd8f31032321c41c7065b6",
+    "url": "/static/media/github.d0fa6e8a.svg"
+  },
+  {
+    "revision": "01381f719b08ed2557c876f34087781c",
+    "url": "/static/media/lock.01381f71.png"
+  },
+  {
+    "revision": "93443885384304e7120e52576d31ac51",
+    "url": "/static/media/logo-circle.93443885.svg"
+  },
+  {
+    "revision": "a27a82bee97cdffb8200bdfe2702a24e",
+    "url": "/static/media/medium.a27a82be.png"
+  },
+  {
+    "revision": "e57e7e1b1f4f58d7bb312ea864917f25",
+    "url": "/static/media/portfolio.e57e7e1b.svg"
+  },
+  {
+    "revision": "72fadc36433844523c2837249f726e02",
+    "url": "/static/media/twitter.72fadc36.svg"
+  }
+]);
