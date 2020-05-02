8ea272d68e0926137301ea44463872006de2a38f
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-05-01 23:09:48 -0700

    Redesign call to action

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -7,14 +7,24 @@ class CallToAction extends Component {
     return (
       <div className="call-to-action">
         <h2 className="call-to-action__header">
-          Lock in the Time-Value of Your Money
+          Deploy a Trustless Fund
         </h2>
+        <h4 className="call-to-action__subheader">
+          Lock in the Time-Value of Your Money 
+        </h4>
         <a 
-          href="https://app.trustless.fund/"
+          href="https://app.trustless.fund/factory"
           className="call-to-action__button"
           target="_blank"
           rel="noopener noreferrer">
           Create Fund
+        </a>
+        <a 
+          className="call-to-action__link"
+          href="https://docs.trustless.fund/"
+          target="_blank"
+          rel="noopener noreferrer">
+            Learn More
         </a>
       </div>
     );
